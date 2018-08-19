ce05707e649c91d57f535c320d87c7b128acd2de
Aline Freitas <aline.do.freitas@gmail.com>
2018-08-19 11:51:25 -0300

    Created relationships between users (followers, followeds)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tweets
+
+
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
+  has_many :followers, through: :active_relationships, source: :followed
+  
+  has_many :passive_relationships, class_name: 'Relationship',
+                                  foreign_key: 'followed_id',
+                                  dependent: :destroy
+  has_many :followeds, through: :passive_relationships, source: :follower
+  
+
+
+
+  def follow(followed_user)
+    unless followed_user.followers.include?(self)
+      followed_user.following(current_user)
+    end
+  end
 end
